25728f225a58cc0c45bc69c49cee68e29080a36b
kelvinmuchui <muchuikelvin423@gmail.com>
2021-08-24 14:53:16 +0300

    First page build

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className =" bg-gray-100 text-gray-600 grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+            
+            <div className = "space-y-4 text-xs text-gray-800">
+                <h5 className ="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className = "space-y-4 text-xs text-gray-800">
+                <h5 className ="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pertty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>KelvinWorks</p>
+            </div>
+            <div className = "space-y-4 text-xs text-gray-800">
+                <h5 className ="font-bold">HOST</h5>
+                <p>Miter Works</p>
+                <p>Presents</p>
+                <p>React Guru hero</p>
+                <p>React on fire</p>
+                <p>Join Now</p>
+            </div>
+            <div className = "space-y-4 text-xs text-gray-800">
+                <h5 className ="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust &  Safety</p>
+                <p>Say Hi to me</p>
+                <p>Easter Eggs</p>
+                <p>For hire</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
